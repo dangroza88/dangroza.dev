d883224aebcf934b557307f7e2749aaed77b4379
dangroza88 <dangroza1988@gmail.com>
2022-06-21 00:30:27 +0300

    logo fix and css update

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -16,6 +16,7 @@ const About = () => {
                 <p className="about-desc1">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nemo vitae obcaecati minima numquam ratione!
                 </p>
+                <br />
                 <p className="about-desc2">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, laboriosam atque ducimus neque, culpa assumenda quod molestiae necessitatibus enim consequatur omnis dolores optio ut quidem reprehenderit eaque? Earum quos error numquam laudantium debitis dolores veritatis ad quidem corrupti! Veniam, unde?
                 </p>
