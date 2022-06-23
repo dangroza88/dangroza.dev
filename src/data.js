8078bcf952a7bee0f1bee76e009d6bec1bf45449
dangroza88 <dangroza1988@gmail.com>
2022-06-23 23:09:13 +0300

    fix case sensitive content for git

--- src/data.js
+++ src/data.js
@@ -2,35 +2,35 @@
 export const projects = [
     {
         id: 1,
-        img: "./src/components/img/commentBox.png",
+        img: "./src/components/img/commentbox.png",
         title: "Comment Box",
         description: "lorem bla bla bla ",
         link: "http://www.patience-is-a-virtue.org/"
     },
     {
         id: 2,
-        img: "./src/components/img/monsterShooter.png",
+        img: "./src/components/img/monstershooter.png",
         title: "Monster Shooter Game",
         description: "lorem bla bla bla ",
         link: "http://www.patience-is-a-virtue.org/"
     },
     {
         id: 3,
-        img: "./src/components/img/privateRyan.png",
+        img: "./src/components/img/privateryan.png",
         title: "Saving Private Ryan Game",
         description: "lorem bla bla bla ",
         link: "http://www.patience-is-a-virtue.org/"
     },
     {
         id: 4,
-        img: "./src/components/img/todoList.png",
+        img: "./src/components/img/todolist.png",
         title: "To Do List",
         description: "lorem bla bla bla ",
         link: "http://www.patience-is-a-virtue.org/"
     },
     {
         id: 5,
-        img: "./src/components/img/visitRome.png",
+        img: "./src/components/img/visitrome.png",
         title: "Visite Rome Website",
         description: "lorem bla bla bla ",
         link: "http://www.patience-is-a-virtue.org/"
