c9f5f87b62775b2fc8b486cbe38ea8414fb48ccd
dangroza88 <dangroza1988@gmail.com>
2022-06-24 22:06:44 +0300

    add test img

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@
 export const projects = [
     {
         id: 1,
-        img: "./src/components/img/comment.jpg",
+        img: "./src/components/img/cbox.png",
         title: "Comment Box",
         description: "lorem bla bla bla ",
         link: "http://www.patience-is-a-virtue.org/"
